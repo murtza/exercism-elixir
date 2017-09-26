c16bf2cd2fc43a8af6b469983090f3dc3789b247
Syed Murtza <syed.murtza@gmail.com>
2017-09-26 15:24:49 +0500

    Solve: bob

--- bob/bob.exs
+++ bob/bob.exs
@@ -1,8 +1,23 @@
 defmodule Bob do
   def hey(input) do
     cond do
-        true -> raise "Your implementation goes here"
+        questioning?(input) -> "Sure."
+        silence?(input) -> "Fine. Be that way!"
+        shouting?(input) -> "Whoa, chill out!"
+        true -> "Whatever."
 
     end
+  end
+
+  defp questioning?(input) do
+    String.ends_with?(input, "?")
+  end
+
+  defp shouting?(input) do
+    String.upcase(input) == input && String.downcase(input) != input
+  end
+
+  defp silence?(input) do
+    String.trim(input) == ""
   end
 end
