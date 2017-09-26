51fd864c8035dd7cbb4566f6db8a5b36117b6f2e
Syed Murtza <syed.murtza@gmail.com>
2017-09-26 19:52:13 +0500

    Solve: raindrops

--- raindrops/raindrops.exs
+++ raindrops/raindrops.exs
@@ -10,6 +10,17 @@ defmodule Raindrops do
   """
   @spec convert(pos_integer) :: String.t
   def convert(number) do
-
+    Enum.map([3,5,7], fn(x) -> do_convert(number, x) end)
+    |> Enum.join
+    |> coalesce(Integer.to_string(number))
   end
+
+  defp coalesce("", fallback), do: fallback
+  defp coalesce(nil, fallback), do: fallback
+  defp coalesce(string, _fallback), do: string
+
+  defp do_convert(number, 3) when rem(number, 3) == 0, do: "Pling"
+  defp do_convert(number, 5) when rem(number, 5) == 0, do: "Plang"
+  defp do_convert(number, 7) when rem(number, 7) == 0, do: "Plong"
+  defp do_convert(number, _), do: ""
 end
