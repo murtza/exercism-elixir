2666e7d6261451050335de827ead79cca34b8f00
Syed Murtza <syed.murtza@gmail.com>
2017-09-26 17:46:57 +0500

    Solve: isogram

--- nth-prime/nth_prime.exs
+++ nth-prime/nth_prime.exs
@@ -4,7 +4,34 @@ defmodule Prime do
   Generates the nth prime.
   """
   @spec nth(non_neg_integer) :: non_neg_integer
-  def nth(count) do
+  def nth(0), do: raise "zero is invalid"
+  def nth(count), do: do_nth(count, 1)
+
+  defp do_nth(0, val), do: val
+  defp do_nth(count, val) do
+    val2 = val + 1
+    case is_prime?(val2) do
+      true ->
+        count2 = count - 1
+        do_nth(count2, val2)
+      false ->
+        do_nth(count, val2)
+    end
+  end
+
+  defp is_prime?(2), do: true
+  defp is_prime?(num) do
+    Enum.all?(2..middle_point(num), &(divide_able?(&1, num)))
+  end
+
+  defp divide_able?(divisor, num) do
+    rem(num, divisor) != 0
+  end
+
+  defp middle_point(num) do
+    num / 2
+    |> Float.ceil
+    |> round
   end
 
 end
