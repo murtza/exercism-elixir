e7841a9440a7bb3b443377d7c7254780468e6963
Syed Murtza <syed.murtza@gmail.com>
2017-09-26 14:25:04 +0500

    initial commit

---
+++ queen-attack/queen_attack.exs
@@ -0,0 +1,30 @@
+defmodule Queens do
+  @type t :: %Queens{ black: {integer, integer}, white: {integer, integer} }
+  defstruct black: nil, white: nil
+
+  @doc """
+  Creates a new set of Queens
+  """
+  @spec new() :: Queens.t()
+  @spec new({integer, integer}, {integer, integer}) :: Queens.t()
+  def new(white, black) do
+
+  end
+
+  @doc """
+  Gives a string reprentation of the board with
+  white and black queen locations shown
+  """
+  @spec to_string(Queens.t()) :: String.t()
+  def to_string(queens) do
+
+  end
+
+  @doc """
+  Checks if the queens can attack each other
+  """
+  @spec can_attack?(Queens.t()) :: boolean
+  def can_attack?(queens) do
+
+  end
+end
